60cb805d04f52189461e06e982b348710722bb14
WillMatthews <will.a.matthews@me.com>
2024-04-13 15:35:43 +0100

    make this a bit more formal, and re-usable

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
 
-# Fight back against scammers
+# knifeFight - Fight back against scammers
 
 Scam text messages are fairly common these days, and the idea of them utterly disgusts me, they prey on people who are vulnerable.
 Similar to the vein of my other work [^1] in calling out scammers / highlighting how bad they are, I give you a tool to fight back.
+In fact, the reason why this tool is called `knifeFight` is due to my disdain for predatory organisations.
 
-`blaster.py` is a script which sends a boatload of fake data to scammers, filling up their database with garbage so it's a pain in the ass for them and may discourage them in the future (I hope).
+`knifeFight.py` is a script which sends a boatload of fake data to scammers, filling up their database with garbage so it's a pain in the ass for them and may discourage them in the future (I hope).
 
-So far you have to manually do the scam link, track the POST requests you send to them with browser dev tools (using the network tab - the pattern I noticed is it's always a few POST request).
-Then copy the post request data and urls into `blaster.py` and use it to spam with fake data to your heart's content
+So far you have to begin by manually doing the scam link, trackng the POST requests you send to them with browser dev tools (using the network tab - the pattern I noticed is it's always a few POST request).
+Then copy the post request data and urls into `knifeFight.py` and use it to spam with fake data to your heart's content
+
+
+## Future Vision
+
+My future vision for this tool is that you should be able to paste a link in, it will figure everything out, and then will blast them into the ground.
+No need for faffing about with tracking POST requests.
 
 
 ## Caveats
 
 Currently this is kind of garbage and requires some hacking to get it working for each case. I know how bad this is, and will fix it up soon.
+
 
 ## Todos
 - [ ] Multiprocessing for more spamming
@@ -28,6 +36,6 @@ Currently this is kind of garbage and requires some hacking to get it working fo
 
 Don't blast every single text - one time they tried to binary search for me after I did six in a row, and I just stopped. They sent approx 12 scam texts in an hour timespan.
 
-[^1]: 'Stop Knife Crime' is an organisation which is known for [harassing and intimidating the public to force them into making donations.](https://docere.uk/articles/the-london-knife-crime-activists)
-I turn the gun on them whenever our paths unfortuantely cross. Call them out. Spend 15 minutes of your day making sure someone doesn't get ripped off.
+[^1]: 'Stop Knife Crime' is a 'cause' which is run by Inside Success UK, a sinister, predatory organisation which is known for [harassing and intimidating the public to force them into making donations.](https://docere.uk/articles/the-london-knife-crime-activists)
+I turn the gun on them whenever our paths unfortuantely cross. Call them out. Spend 15 minutes of your day making sure someone doesn't get ripped off, we all deserve someone to stand up for us.
 I'm writing a blog post about this soon, with some of my investigative work.
--- blast.py
+++ blast.py
@@ -8,117 +8,161 @@ import json
 import csv
 import time
 
+# Go to https://www.fakenamegenerator.com/order.php and download a CSV file of identities.
+# You might as well get the maximum quantity, which is 100,000 (as of 13/04/24).
+# Include ALL fields, and make sure to choose an appropriate name set and country.
+# Extract the CSV file and put it in the same directory as this script, and change the filename below.
+datafile = "FakeNameGenerator.com_da48c5a0.csv"
 
-"""
-1Number,
-2Gender,
-3NameSet,
-4Title,
-5GivenName,
-6MiddleInitial,
-7Surname,
-8StreetAddress,
-9City,
-10State,
-11StateFull,
-12ZipCode,
-13Country,
-14CountryFull,
-15EmailAddress,
-16Username,
-17Password,
-18BrowserUserAgent,
-19TelephoneNumber,
-20TelephoneCountryCode,
-21MothersMaiden,
-22Birthday,
-23Age,
-24TropicalZodiac,
-25CCType,
-26CCNumber,
-27CVV2,
-28CCExpires,
-"""
+def associateFields(filename: str) -> dict:
+    """associateFields reads the first row of a csv file and returns a dictionary associating each field with its index."""
+    first_row = []
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            first_row = row
+            break
+    fields = {}
+    for i, field in enumerate(first_row):
+        fields[field] = i
+    return fields
 
+def parseFakeNameGen(filename: str) -> dict:
+    """parseFakeNameGen is a generator that reads a csv file from FakeNameGenerator.com and returns a dictionary of the fields in the next row."""
+    fields = associateFields(filename)
+    print(fields)
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            identity = {}
+            for f in dict.keys(fields):
+                identity[f] = row[fields[f]]
+            yield identity
 
-start_val =  random.randint(0,90000)
 passwords = json.loads(open('pwds.json').read())
+def randomPassword() -> str:
+    """randomPassword generates a random password.."""
+    passwd = random.choice(passwords)
+    randsym = random.choice(["!","%","$","&","£","!","!","#","-","*","","","","","","","","",""])
+    randnum = random.choice([random.randint(0,9),""])
+    passwd = passwd.capitalize()+str(randnum)
+    passwd = random.choice([passwd, passwd+randsym])
+    return passwd
+
 user_agents = json.loads(open('uas.json').read())
+def fakeClient() -> {"ip":str, "headers":dict, "screen":str}:
+    randip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+    headers = {"User-Agent":random.choice(user_agents)}
+    screen = random.choice(['414+x+736', '375+x+812','375+x+667'])
+    return {"ip":randip, "headers":headers, "screen":screen}
+
 counties = json.loads(open('counties.json').read())
+def randomCounty() -> str:
+    return random.choice(counties)
 
-itervar = 1
-with open("./FakeNameGenerator.com_e75ab0f0.csv") as csvfile:
-    reader = csv.reader(csvfile, delimiter=",")
+def logo():
+    print("""
+    ██╗░░██╗███╗░░░██╗██╗███████╗███████╗███████╗██╗░██████╗░██╗░░██╗████████╗
+    ██║░██╔╝████╗░░██║██║██╔════╝██╔════╝██╔════╝██║██╔════╝░██║░░██║╚══██╔══╝
+    █████╔╝░██╔██╗░██║██║█████╗░░█████╗░░█████╗░░██║██║░░███╗███████║░░░██║░░░
+    ██╔═██╗░██║╚██╗██║██║██╔══╝░░██╔══╝░░██╔══╝░░██║██║░░░██║██╔══██║░░░██║░░░
+    ██║░░██╗██║░╚████║██║██║░░░░░███████╗██║░░░░░██║╚██████╔╝██║░░██║░░░██║░░░
+    ╚═╝░░╚═╝╚═╝░░╚═══╝╚═╝╚═╝░░░░░╚══════╝╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░""")
 
-    for i, I in enumerate(reader):
-        if i < 1 + start_val:
+def randomEmail(I: dict) -> str:
+    eml_prefix = random.choice(
+            [I["Username"],
+            I["GivenName"]+"."+I["Surname"],
+            I["GivenName"]+"."+I["Surname"]+I["Birthday"][-3:-1],
+            I["GivenName"]+I["Surname"],
+            I["GivenName"]+I["Surname"]+I["Birthday"][-3:-1],
+            I["GivenName"]+I["Surname"]+I["Birthday"][-2:-1],
+            I["EmailAddress"]
+        ])
+    eml_suffix = random.choice(["yahoo.com", "btinternet.co.uk","gmail.com","icloud.com","outlook.com","hotmail.com","hotmail.co.uk","yahoo.co.uk","aol.com","gmail.com"])
+    email = eml_prefix if "@" in eml_prefix else eml_prefix+"@"+eml_suffix
+    return email
+
+def getCard(I: dict) -> {"number":str, "expiry": {"month":str, "year":str, "mm":str, "yy":str, "mm/yy":str}, "cvv":str, "name":str}:
+    """getCard takes an identity dictionary and returns a dictionary of card details."""
+    exp = I["CCExpires"].split("/")
+    expiry = {
+        "month":exp[0],
+        "year":exp[1],
+        "mm": str(exp[0].zfill(2)),
+        "yy": str(exp[1][-2:]),
+        "mm/yy": str(exp[0].zfill(2))+"/"+str(exp[1][-2:])
+    }
+    return {
+        "number": I["CCNumber"],
+        "expiry": expiry,
+        "cvv": I["CVV2"],
+        "name": I["GivenName"]+" "+I["Surname"]
+    }
+
+def assemblePayloads(I: dict) -> [dict]:
+    """assemblePayloads takes an identity dictionary and returns a list of payloads to be sent to a target site."""
+    client = fakeClient()
+    headers = client["headers"]
+
+    passwd = randomPassword()
+    county = randomCounty()
+    email  = randomEmail(I)
+    card   = getCard(I)
+
+    digits = "".join([str(random.randint(0,9)) for i in range(random.randint(5,16))])
+    username = random.choice([digits, email.split("@")[0], email])
+
+    # Make the payloads. This is the bit you need to do manually, as it's different for each site.
+    # Use the above definitions to fill in the blanks, and see fakeNameFields.md for more fields that FakeNameGenerator.com provides.
+    payload0={'submit':'Start'}
+    payload1={'fname':I["GivenName"]+"+"+I["Surname"],'d1':str(random.randint(1,31)),'d2':str(random.randint(1,31)),'d3':str(random.randint(1942,1999)),'email':email,'con':county,'pc':I["ZipCode"],'town':I["City"],'address':I["StreetAddress"],'address2':'','telephone':I["TelephoneNumber"].strip(),'submit':"Continue"}
+    payload2={'name': I["GivenName"]+"+"+I["Surname"], 'xxx':card["number"], 'expiryY': card["expiry"]["year"], 'expiryM': card["expiry"]["month"], 'an': str(random.randint(10000000,99999999)), 'sn':str(random.randint(10,99))+"-"+str(random.randint(10,99))+"-"+str(random.randint(10,99)), 'xxxxx':I["NationalID"], 'submit':"Continue"}
+    print(payload0)
+    print(payload1)
+    print(payload2)
+
+    config = {
+        payloads: [payload0, payload1, payload2],
+        failRates: [0.0, 0.25, 0.33],
+        target: "https://gov.uk-check.org/refund/"
+        endpoints: ["details", "next2", "next3.php"]
+        logContent: ["Got Session ID", "Sent Fake ID", "Sent Fake CC"]
+    }
+    return config
+
+
+def punish(config: dict):
+    """punish takes a configuration dictionary and sends payloads to a target site,
+    hopefully causing a scammer somewhere to lose resources, time, or their mind.
+    Hopefully, all three."""
+    try:
+        with requests.Session() as S:
+            while j < len(config["payloads"]):
+                r = S.post(target+config["endpoints"][j], headers=headers, data=config["payloads"][j])
+                print(r.json)
+                if random.random() < config["failRates"][j]:
+                    print("Decided to fail", j)
+                    break
+                print(config["logContent"][j])
+                j += 1
+            print("Completed", i, "identities")
+    except Exception as e:
+        print("An Exception Occured. Maybe they're dead.")
+        print(e)
+        time.sleep(10)
+
+def main():
+    ids = parseFakeNameGen(datafile)
+    for i, I in enumerate(ids):
+        if i == 0:
+            logo()
+            # Skip the first identity- it's the header row.
             continue
 
-        # Fake Client Gen
-        randip =".".join(map(str, (random.randint(0, 255)
-                    for _ in range(4))))
-        headers={"User-Agent":random.choice(user_agents)}
-        screen = random.choice(['414+x+736', '375+x+812','375+x+667'])
+        payloads = assemblePayloads(I)
+        punish(payloads)
 
-        # Fake ID Gen
-        county = random.choice(counties)
-        passwd = random.choice(passwords)
-        eml_prefix = random.choice(
-                [I[15],
-                I[4]+"."+I[6],
-                I[4]+"."+I[6]+I[21][-3:-1],
-                I[4]+I[5]+I[6],
-                I[4]+I[5]+I[6]+I[21][-3:-1],
-                I[4]+I[6]+I[21][-2:-1],
-                I[14]
-            ])
-        eml_suffix = random.choice(["yahoo.com", "btinternet.co.uk","gmail.com","icloud.com","outlook.com","hotmail.com","hotmail.co.uk","yahoo.co.uk","aol.com","gmail.com"])
+if __name__ == "__main__":
+    main()
 
-        if "@" not in eml_prefix:
-            email = eml_prefix+"@"+eml_suffix
-        else:
-            email = eml_prefix
-
-        # Fake Username Gen
-        digits = "".join([str(random.randint(0,9)) for i in range(random.randint(5,16))])
-        username = random.choice([digits,eml_prefix,email])
-
-
-        # Fake Password Gen
-        randsym = random.choice(["!","%","$","&","£","!","!","#","-","*","","","","","","","","",""])
-        randnum = random.choice([random.randint(0,9),""])
-        passwd = passwd.capitalize()+str(randnum)
-        passwd = random.choice([passwd, passwd+randsym])
-
-
-        # Make the payload.
-        payload0={'submit':'Start'}
-        payload1 = {'fname':I[4]+"+"+I[6],'d1':str(random.randint(1,31)),'d2':str(random.randint(1,31)),'d3':str(random.randint(1942,1999)),'email':email,'con':county,'pc':I[11],'town':I[8],'address':I[7],'address2':'','telephone':I[18].strip(),'submit':"Continue"}
-        payload2= {'name':I[4]+"+"+I[6], 'xxx':I[25], 'expiryY': str(random.randint(2022,2029)) , 'expiryM': str(random.randint(1,12)), 'an': str(random.randint(10000000,99999999)), 'sn':str(random.randint(10,99))+"-"+str(random.randint(10,99))+"-"+str(random.randint(10,99)), 'xxxxx':I[26], 'submit':"Continue"}
-        print(payload0)
-        print(payload1)
-        print(payload2)
-
-        # Send
-        try:
-            with requests.Session() as S:
-                r1 = S.post("https://gov.uk-check.org/refund/details", headers=headers, data=payload0)
-                print(r1.json)
-                print("GOT SESSID")
-                if random.choice([0,0,0,1]) == 0:
-                    r2 = S.post("https://gov.uk-check.org/refund/next2", headers=headers, data=payload1)
-                    print(r2.json)
-                    print("SENT FAKE ID")
-
-                    if random.choice([0,0,1]) == 0:
-                        r3 = S.post("https://gov.uk-check.org/refund/next3.php",headers=headers,data=payload2)
-                        print(r3.json)
-                        print(i, ": Sent Fake CC")
-
-                print("Completed", itervar, "identities")
-        except Exception as e:
-            print("An Exception Occured. Maybe they're dead.")
-            print(e)
-            time.sleep(10)
-
-        itervar += 1
